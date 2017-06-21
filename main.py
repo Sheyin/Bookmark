5417a2fa8107434a6412b886bb79a4f148e3044e
Sheyin <sheyinkage@gmail.com>
2017-06-21 16:55:25 -0500

    Can retrieve sites from db now

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
-from flask import Flask
-from flask import render_template
-import enginetest
+from flask_pymongo import PyMongo
+from flask import Flask, Response, render_template, request
+import secret
+import datetime
+import pprint	# Make objects look nicer in console
+from pymongo import MongoClient
+db = secret.dbpath
 
 app = Flask(__name__)
+# Configuration for mongo / pymongo
+app.config['MONGO_HOST'] = secret.host
+app.config['MONGO_PORT'] = secret.port
+app.config['MONGO_DBNAME'] = secret.dbname
+mongo = PyMongo(app)
+
+# Displays the form for adding a bookmark
+@app.route("/create.html", methods=['GET', 'POST'])
+@app.route("/create", methods=['GET', 'POST'])
+@app.route("/new", methods=['GET', 'POST'])
+def createBookmark():
+	if request.method == 'GET':
+		return render_template('create.html')
+		# ex. searchword = request.args.get('key', '') - 
+		# retrieves url parameters(?key=value)
+	elif request.method == 'POST':
+		# might be request.form.name
+		name = request.form['name']
+		url = request.form['url']
+		description = request.form['description']
+		tags = request.form['tags']
+		addedBy = request.form['addedBy']
+		print ("Name: " + name + " url: " + url)
+		print ("Description: " + description + " Tags: " + str(tags))
+		print ("Added by: " + addedBy)
+		# Send data to db
+		# Return some response
+		return render_template('added.html', name=name)
+
+@app.route("/get")
+def retrieveSite():
+	searchword = request.args.get('key', '')
+	site = db.find_one({"name": searchword})
+	#site = mongo.db.collectionname.find_one_or_404({'name': searchword})
+	#pprint.pprint("site: " + str(site))
+	if not site:
+		return render_template('notfound.html', searchword=searchword)
+	else:
+		return render_template('found.html', name=site['name'], url=site['url'], dateVisited=site['dateVisited'])
 
 @app.route("/")
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-@app.route("/dead-in-the-water/")
-def deadInTheWater():
-	# What the console says
-	console = enginetest.bottomLevelTest()
-	return render_template('main.html', input=input)
-
-@app.route("/dead-in-the-water/<input>")
-def deadInTheWaterInput(input=None):
-	return render_template('main.html', input=input)
+def showHub():
+	return render_template('hub.html')
 
 if __name__ == "__main__":
     app.run()
