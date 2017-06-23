9021d67da72dd6bb2aeaf1b80693a21a52928bf8
Sheyin <sheyinkage@gmail.com>
2017-06-22 19:57:40 -0500

    Search by name fully operational

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ mongo = PyMongo(app)
 def createBookmark():
 	if request.method == 'GET':
 		return render_template('create.html')
-		# ex. searchword = request.args.get('key', '') - 
+		# ex. searchword = request.args.get('key', '') 
 		# retrieves url parameters(?key=value)
 	elif request.method == 'POST':
 		# might be request.form.name
@@ -40,36 +40,76 @@ def createBookmark():
 
 @app.route("/get")
 def retrieveSite():
-	searchword = request.args.get('key', '')
-	#searchterm = "/g/"
-	#site = db.find({"name": searchterm})
-	regex = re.compile('\w*' + searchword + '\w*')
-	site = db.find({"name": {"$regex": regex}})
-	# Just for debugging
-	name = ""
-	url = ""
-	desciption = ""
-	date = ""
-	results = []
-	for doc in site:
-		name = doc['name']
-		url = doc['url']
-		description = doc['description']
-		date = doc['dateVisited']
-		results.append((name, url, description, date))
-		print(str(doc))
-	if site.count() == 1:
-		return render_template('found.html', name=name, url=url, description=description)
-	elif site.count() >= 1:
-		return render_template('foundTag.html', results=results)
+	# Might need to limit combinations until a better way to pass arguments
+	# is learned
+	searchTerm = request.args.get('name', '') 
+	
+	if not searchTerm:
+		return render_template('search.html')
 	else:
-		return render_template('notfound.html')
+		regex = re.compile('\w*' + searchTerm + '\w*')
+		site = ""
+		# complete working query:
+		site = db.find({"name": {"$regex": regex}})
+		#site = db.find({"$text": {"$search": searchTerm }})
+		# debug - Error: text index required for $text query
+		name = ""
+		url = ""
+		desciption = ""
+		date = ""
+		results = []
+		for doc in site:
+			name = doc['name']
+			url = doc['url']
+			description = doc['description']
+			date = doc['dateVisited']
+			results.append((name, url, description, date))
+			print(str(doc))
+		if site.count() == 1:
+			return render_template('found.html', name=name, url=url, description=description)
+		elif site.count() >= 1:
+			return render_template('found.html', results=results)
+		else:
+			return render_template('found.html', searchword=searchword)
 		
 
 
 @app.route("/")
 def showHub():
 	return render_template('hub.html')
+
+
+# This is the experimental code to get multiple field searches
+def searchMultiple():
+	# List of variables to get from request url
+	'''
+	searchName = ("name", request.args.get("name"))
+	searchUrl = ("url", request.args.get("url"))
+	searchTag = ("tags", request.args.get("tag"))
+	searchDescription = ("description", request.args.get("description"))
+	searchDateRange = ("dateVisited", request.args.get("dateRange"))
+	#regex2 = re.compile('\w*' + 'programming' + '\w*')
+	#site = db.find({"name": {"$regex": regex}, "tags": {"$regex": regex2}})
+
+	regphrase = '": {$regex: '
+	searchList = [searchName, searchUrl, searchTag, searchDescription, searchDateRange]
+	query = "{"
+	reglist = []
+	querylist = []
+	separator = '}, '
+
+	for searchTerm in searchList:
+		print (searchTerm)
+		if searchTerm[1] != None:
+			print (searchTerm[1])
+			reglist.append(re.compile('\w*' + searchTerm[1] + '\w*'))
+			#regex = re.compile('\w*' + searchTerm[1] + '\w*')
+			query += '\"' + searchTerm[0] + regphrase
+	# end query string with bracket
+	query += "}"
+	'''
+	return
+
 
 if __name__ == "__main__":
     app.run()
